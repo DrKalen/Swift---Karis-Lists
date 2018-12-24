28830973edf3d1c503c29e78cfe8f8631ea65239
Kalen Hammann <kalen7@gmail.com>
2018-12-24 14:40:41 -0500

    set up 4 view controllers in xib + tableView

--- Karis Lists/ViewController.swift
+++ Karis Lists/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var data: [String] = ["One", "Two", "Three"]
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let dataObject = UserDefaults.standard.object(forKey: "data")
+//        data = dataObject as? [String] ?? [String]()
+//        table.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            data.remove(at: indexPath.row)
+            UserDefaults.standard.set(data, forKey: "data")
+            table.reloadData()
+        }
+    }
+
 
 
 }
