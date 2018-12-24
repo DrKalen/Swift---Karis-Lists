d1379b3a623c728edaa7e13f7490a6cb4f21184c
Kalen Hammann <kalen7@gmail.com>
2018-12-24 15:24:59 -0500

    Laid out UI - wip

---
+++ AddFoodViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddFoodViewController.swift
+//  Karis Lists
+//
+//  Created by Kalen Hammann on 12/24/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import UIKit
+
+class AddFoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
